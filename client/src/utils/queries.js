96b47d63a9c825ae6d0706c55a7d25cfbdce4ffd
JosephJamesCoop <cooper_3869@live.com>
2022-04-12 13:59:36 -0700

    inputed known queries and mutations

---
+++ client/src/utils/queries.js
@@ -0,0 +1,83 @@
+import { gql } from "@apollo/client";
+
+export const QUERY_MALLS = gql`
+  {
+    Mall {
+      _id
+      mallName
+      style
+      location
+      Address
+      Store {
+      storeName
+      image
+      description
+      URL
+      category {
+        _id
+        name
+      }
+    }
+  }
+`;
+
+export const QUERY_STORES = gql` 
+  {
+    Store {
+      mallID
+      storeName
+      image
+      description
+      URL
+      category {
+        _id
+        name
+      }
+    }
+  }
+`;
+
+export const QUERY_PRODUCTS = gql`
+  query getProducts($category: ID) {
+    products(category: $category) {
+      _id
+      name
+      description
+      price
+      quantity
+      image
+      category {
+        _id
+        name
+      }
+    }
+  }
+`;
+
+export const QUERY_CATEGORIES = gql`
+  {
+    categories {
+      _id
+      name
+    }
+  }
+`;
+
+export const QUERY_USER = gql`
+  {
+    user {
+      username
+      email
+      malls {
+        _id
+        mallName
+        style
+        location
+        Address
+        stores {
+          storeName
+        }
+      }
+    }
+  }
+`;
