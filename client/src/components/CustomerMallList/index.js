041555e89d816f7033f7f5e901ec30f40a6e7752
JosephJamesCoop <cooper_3869@live.com>
2022-04-13 11:19:22 -0700

    complete except filter and generate to stores section onClick

--- client/src/components/CustomerMallList/index.js
+++ client/src/components/CustomerMallList/index.js
@@ -1,48 +1,49 @@
-import React from 'react';
-import { useQuery } from '@apollo/client';
-import { QUERY_MALLS } from '../utils/queries';
+import React from "react";
+import { useQuery } from "@apollo/client";
+import { QUERY_MALLS } from "../utils/queries";
 
 function CustomerMallList() {
-
   const { Mall } = useQuery(QUERY_MALLS);
- const style = Mall.style
+
+  const locations = Mall.location;
+
+  let unique = [...new Set(locations)];
+
   return (
-<>
-  <section>
-    <div className="box margin50">
-      <div className="box inline margin50">
-        <h2 className="center">List of Malls</h2>
-        <div className="center">
-          <input className="center" list="select" name="selectCity" placeholder="Select A Category"></input>
-          <datalist className="center" id="select">  
-              <option value="Plaza" ></option>
-              <option value="Strip Mall1"></option>
-              <option value="Shopping Center"></option>
-          </datalist>
-        
-        {/* This is saved for updating to location
-          <datalist className="center" id="select">  
-            {location.map(({ name }) => (
-              <option value={ name }></option>
-              ))}
-          </datalist> */}
-        
-        
+    <>
+      <section>
+        <div className="box margin50">
+          <div className="box inline margin50">
+            <h2 className="center">List of Malls</h2>
+            <div className="center">
+              <input
+                className="center"
+                list="select"
+                name="selectCity"
+                placeholder="Select A Category"
+              ></input>
+              {/* Maps over locations for drop down filtering */}
+              <datalist className="center" id="select">
+                {unique.map((loc) => (
+                  <option value={loc}></option>
+                ))}
+              </datalist>
+            </div>
+          </div>
+          {/* TODO: create filtration in response to input value above */}
+          <ul className="scrollBox">
+            {Mall.map(({ mallName, style, location, _id }) => (
+              // TODO: create an onClick() funtion that will generate the stores array in the stores field.
+              <li className="box center" id={_id}>
+                <h4>{mallName} </h4>
+                <p>Style: {style}</p>
+                <p>Location: {location}</p>
+              </li>
+            ))}
+          </ul>
         </div>
-      </div>
-      <ul className="scrollBox">
-        {Mall.map(({ mallName, style, location, _id}) => (
-// Need an onClick() funtion that will generate the stores array in the stores field.
-        <li className="box center" id={_id}>
-          <h4>{mallName} </h4>
-          <p>Style: {style}</p>
-          <p>Location: {location}</p>
-        </li>
-        ))}
-      </ul>
-    </div>
-  </section>
-</>
+      </section>
+    </>
   );
 }
 
