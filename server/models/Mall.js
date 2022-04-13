89181ba8f9161b02955c178062cbc86cd676e4b0
Zachary Clark <zacclark369@gmail.com>
2022-04-12 19:33:48 -0700

    finished Mall model

--- server/models/Mall.js
+++ server/models/Mall.js
@@ -1,9 +1,13 @@
 const mongoose = require('mongoose');
 const {Schema} = mongoose;
-const bcrypt = require('bcrypt');
+// const bcrypt = require('bcrypt');
 
 const mallSchema = new Schema({
-
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    }
 });
 
 const Mall = mongoose.model('Mall', mallSchema);
