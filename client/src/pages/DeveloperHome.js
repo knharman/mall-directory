e60ff3ea1bcbf4c8d41f796769391e56b3c44de9
CLAW <christopher.lawn@yahoo.com>
2022-04-12 21:03:52 -0700

    updated components, pages, App.js, and index.css files

---
+++ client/src/pages/DeveloperHome.js
@@ -0,0 +1,10 @@
+import React from "react";
+import DeveloperMallList from "../components/DeveloperMallList";
+
+const DeveloperHome = () => {
+    return (
+        <DeveloperMallList />
+    )
+};
+
+export default DeveloperHome;
