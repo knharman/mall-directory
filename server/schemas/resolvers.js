b25060fa2712a7d61ccdf0040e0fc38ba673048a
Zachary Clark <zacclark369@gmail.com>
2022-04-12 17:50:29 -0700

    started resolvers and created user model

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,8 +1,23 @@
-
+const {AuthenticationError} = require('apollo-server-express');
+const {signToken} = require('../utils/auth');
+const {User} = require('../models');
 
 
 const resolvers = {
-
+    user: async (parent, args, context) => {
+        if (context.user) {
+          const user = await User.findById(context.user._id).populate({
+            path: "",
+            populate: "malls",
+          });
+  
+          user.orders.sort((a, b) => b.purchaseDate - a.purchaseDate);
+  
+          return user;
+        }
+  
+        throw new AuthenticationError("Not logged in");
+      },
 
 };
 
