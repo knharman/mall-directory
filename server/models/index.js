f3c33ac2580f1aa5abc55e776cef61807f590688
Zachary Clark <zacclark369@gmail.com>
2022-04-12 19:35:02 -0700

    updated index.js for models

--- server/models/index.js
+++ server/models/index.js
@@ -1,4 +1,6 @@
 const User = require('./User');
 const Mall = require('./Mall');
+const Store = require('./Store');
 
-module.exports = { User, Product, };
+
+module.exports = { User, Store, Mall };
