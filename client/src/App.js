d9ba60c6d8e2ca0e0389a6d4dbefdefd74d5dc7a
CLAW <christopher.lawn@yahoo.com>
2022-04-13 20:09:15 -0700

    created/styled/tested contact form component & successfully sent messages through it

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
-import DeveloperHome from './pages/DeveloperHome';
+// import DeveloperHome from './pages/DeveloperHome';
+import ContactUs from './pages/ContactUs';
 
 function App() {
   return (
-    <DeveloperHome />
+    <>
+    {/* <DeveloperHome /> */}
+    <ContactUs />
+    </>
   );
 }
 
