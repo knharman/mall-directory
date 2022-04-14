92ceac5b9ae823cf83269b023287e62aec21cb8b
CLAW <christopher.lawn@yahoo.com>
2022-04-14 00:13:28 -0700

    created & styled DeveloperLoginForm component.  Added email validation & password input check.

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,13 @@
+import { gql } from '@apollo/client';
+
+export const DEVELOPER_LOGIN = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
