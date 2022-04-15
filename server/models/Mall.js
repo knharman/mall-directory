4969371d854d680f3dfe970968cf3abf969713e0
Zachary Clark <zacclark369@gmail.com>
2022-04-14 19:36:57 -0700

    changes for server and query

--- server/models/Mall.js
+++ server/models/Mall.js
@@ -7,7 +7,8 @@ const mallSchema = new Schema({
         type: String,
         required: true,
         trim: true
-    }
+    },
+    stores: [Store]
 });
 
 const Mall = mongoose.model('Mall', mallSchema);
