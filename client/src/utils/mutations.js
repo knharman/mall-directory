98e093cd6c84c040b809bb150fa46d38bc42bc3d
Kellie Harman <knharman@gmail.com>
2022-04-17 11:14:18 -0700

    all mall and developer mutations tested and functioning

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -49,3 +49,45 @@ export const ADD_MALL = gql`
     }
   }
 `;
+
+export const UPDATE_MALL = gql`
+mutation UpdateMall($id: ID!, $mallName: String!, $style: String!, $location: String!) {
+  updateMall(_id: $id, mallName: $mallName, style: $style, location: $location) {
+    mallName
+    location
+    _id
+    style
+    stores {
+      storeName
+      image
+      category {
+        _id
+        name
+      }
+      description
+      url
+    }
+  }
+}
+`;
+
+export const DELETE_MALL = gql`
+mutation RemoveMall($id: ID!) {
+  removeMall(_id: $id) {
+    _id
+    mallName
+    style
+    location
+    stores {
+      storeName
+      image
+      description
+      url
+      category {
+        _id
+        name
+      }
+    }
+  }
+}
+`;
