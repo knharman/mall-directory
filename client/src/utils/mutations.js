1c179e707b53ba8cdc658e8290c124ea5d1dd54a
Kellie Harman <knharman@gmail.com>
2022-04-17 10:11:46 -0700

    addMall now works

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -11,3 +11,41 @@ export const DEVELOPER_LOGIN = gql`
     }
   }
 `;
+
+export const ADD_USER = gql`
+  mutation addUser(
+    $username: String!
+    $email: String!
+    $password: String!
+  ) {
+    addUser(
+      username: $username
+      email: $email
+      password: $password
+    ) {
+      token
+      user {
+        _id
+      }
+    }
+  }
+`;
+
+export const ADD_MALL = gql`
+  mutation addMall(
+    $mallName: String!
+    $style: String!
+    $location: String!
+  ) {
+    addMall(
+      mallName: $mallName
+      style: $style
+      location: $location
+    ) {
+      token
+      user{
+        _id
+      }
+    }
+  }
+`;
