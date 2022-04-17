61b691341e673242ec850cd00845ea3e9fea3b9d
CLAW <christopher.lawn@yahoo.com>
2022-04-16 22:18:59 -0700

    set up Apollo router/paths in main App.js file to point to each page

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,46 @@
 import './App.css';
+import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+import CustomerHome from './pages/CustomerHome'
 import ContactUs from './pages/ContactUs';
 import DeveloperLogin from './pages/DeveloperLogin';
+import DeveloperHome from './pages/DeveloperHome'
+
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
     <>
-    <DeveloperLogin />
-    <ContactUs />
+      <ApolloProvider client={client}>
+        <Router>
+          <Switch>
+            <Route exact path="/" component={CustomerHome} />
+            <Route exact path="/login" component={DeveloperLogin} />
+            <Route exact path="/contact" component={ContactUs} />
+            <Route exact path="/dashboard" component={DeveloperHome} />
+          </Switch>
+        </Router>
+      </ApolloProvider>
     </>
-
   );
 }
 
