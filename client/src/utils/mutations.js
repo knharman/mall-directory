696cd7d227f44e9f1076af837580814df1151934
Kellie Harman <knharman@gmail.com>
2022-04-17 11:43:48 -0700

    added front end mutations and formatted them

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -51,34 +51,50 @@ export const ADD_MALL = gql`
 `;
 
 export const UPDATE_MALL = gql`
-mutation UpdateMall($id: ID!, $mallName: String!, $style: String!, $location: String!) {
-  updateMall(_id: $id, mallName: $mallName, style: $style, location: $location) {
-    mallName
-    location
-    _id
-    style
-    stores {
-      storeName
-      image
-      category {
-        _id
-        name
+  mutation UpdateMall($id: ID!, $mallName: String!, $style: String!, $location: String!) {
+    updateMall(_id: $id, mallName: $mallName, style: $style, location: $location) {
+      mallName
+      location
+      _id
+      style
+      stores {
+        storeName
+        image
+        category {
+          _id
+          name
+        }
+        description
+        url
       }
-      description
-      url
     }
   }
-}
 `;
 
-export const DELETE_MALL = gql`
-mutation RemoveMall($id: ID!) {
-  removeMall(_id: $id) {
-    _id
-    mallName
-    style
-    location
-    stores {
+export const REMOVE_MALL = gql`
+  mutation RemoveMall($id: ID!) {
+    removeMall(_id: $id) {
+      _id
+      mallName
+      style
+      location
+      stores {
+        storeName
+        image
+        description
+        url
+        category {
+          _id
+          name
+        }
+      }
+    }
+  }
+`;
+
+export const ADD_STORE = gql`
+  mutation AddStore($storeName: String!, $image: String!, $category: String!, $description: String!, $url: String!, $mallId: ID!) {
+    addStore(storeName: $storeName, image: $image, category: $category, description: $description, url: $url, mallID: $mallId) {
       storeName
       image
       description
@@ -89,5 +105,46 @@ mutation RemoveMall($id: ID!) {
       }
     }
   }
-}
+`;
+
+export const UPDATE_STORE = gql`
+  mutation UpdateStore($mallId: ID!, $storeId: ID!, $storeName: String!, $image: String!, $category: String!, $description: String!, $url: String!) {
+    updateStore(mallID: $mallId, storeID: $storeId, storeName: $storeName, image: $image, category: $category, description: $description, url: $url) {
+      _id
+      mallName
+      style
+      location
+      stores {
+        storeName
+        image
+        category {
+          _id
+          name
+        }
+        description
+        url
+      }
+    }
+  }
+`;
+
+export const REMOVE_STORE = gql`
+  mutation RemoveStore($mallId: ID!, $storeId: ID!) {
+    removeStore(mallID: $mallId, storeID: $storeId) {
+      _id
+      mallName
+      style
+      location
+      stores {
+        storeName
+        image
+        description
+        url
+        category {
+          _id
+          name
+        }
+      }
+    }
+  }
 `;
